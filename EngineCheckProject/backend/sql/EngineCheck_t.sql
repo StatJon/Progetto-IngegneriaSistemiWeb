a7b4ee1c2b56c7f77a6070acac4983a2c59ddbfb
Jonathan Crescentini <jonathan.crescentini@studio.unibo.it>
2025-12-04 16:37:18 +0100

    style: EngineCheck_t.sql translated to english

--- EngineCheckProject/backend/sql/EngineCheck_t.sql
+++ EngineCheckProject/backend/sql/EngineCheck_t.sql
@@ -1,60 +1,60 @@
 /* Tables */
 ``
 
-CREATE TABLE `DIPENDENTE` (
-    `N_Badge` int NOT NULL AUTO_INCREMENT,
-    `Nome` varchar NOT NULL,
-    `Cognome` varchar NOT NULL,
+CREATE TABLE `EMPLOYEE` (
+    `Badge_Number` int NOT NULL AUTO_INCREMENT,
+    `First_Name` varchar NOT NULL,
+    `Last_Name` varchar NOT NULL,
     `Password` varchar NOT NULL,
-    PRIMARY KEY (`N_Badge`)
+    PRIMARY KEY (`Badge_Number`)
 );
-CREATE TABLE `CLIENTE` (
+CREATE TABLE `CUSTOMER` (
     `Email` varchar NOT NULL,
-    `Nome` varchar NOT NULL,
-    `Cognome` varchar NOT NULL,
+    `First_Name` varchar NOT NULL,
+    `Last_Name` varchar NOT NULL,
     `Password` varchar NOT NULL,
-    `Telefono` int(10) NOT NULL,
+    `Phone` int(10) NOT NULL,
     PRIMARY KEY (`Email`)
 );
-CREATE TABLE `LAVORO` (
-    `ID_Lavoro` int NOT NULL AUTO_INCREMENT,
-    `DurataPrevista` int NOT NULL,
-    `Titolo` varchar NOT NULL,
-    `Descrizione` varchar NOT NULL,
-    `Prezzo` int NOT NULL,
-    PRIMARY KEY (`ID_Lavoro`)
+CREATE TABLE `JOB` (
+    `Job_ID` int NOT NULL AUTO_INCREMENT,
+    `Estimated_Duration` int NOT NULL,
+    `Title` varchar NOT NULL,
+    `Description` varchar NOT NULL,
+    `Price` int NOT NULL,
+    PRIMARY KEY (`Job_ID`)
 );
-CREATE TABLE `APPUNTAMENTO` (
-    `ID_Appuntamento` int NOT NULL AUTO_INCREMENT,
-    `modello` varchar NOT NULL,
-    `tipoAutoMoto` int NOT NULL,
-    `targa` varchar(7) NOT NULL,    
-    `data_ora` DATETIME NOT NULL,
-    `CLIENTE_Email` varchar NOT NULL,
-    PRIMARY KEY (`ID_Appuntamento`),
-    FOREIGN KEY (`CLIENTE_Email`) REFERENCES `CLIENTE` (`Email`)
+CREATE TABLE `APPOINTMENT` (
+    `Appointment_ID` int NOT NULL AUTO_INCREMENT,
+    `Model` varchar NOT NULL,
+    `Vehicle_Type` int NOT NULL,
+    `License_Plate` varchar(7) NOT NULL,    
+    `Date_Time` DATETIME NOT NULL,
+    `CUSTOMER_Email` varchar NOT NULL,
+    PRIMARY KEY (`Appointment_ID`),
+    FOREIGN KEY (`CUSTOMER_Email`) REFERENCES `CUSTOMER` (`Email`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION
 );
-CREATE TABLE `COMPOSTO_DA` (
-    `APPUNTAMENTO_ID_Appuntamento` int NOT NULL ,
-    `LAVORO_ID_Lavoro` int NOT NULL ,
-    PRIMARY KEY (`APPUNTAMENTO_ID_Appuntamento`, `LAVORO_ID_Lavoro`),
-    FOREIGN KEY (`APPUNTAMENTO_ID_Appuntamento`) REFERENCES `APPUNTAMENTO` (`ID_Appuntamento`)
+CREATE TABLE `COMPOSED_OF` (
+    `APPOINTMENT_Appointment_ID` int NOT NULL ,
+    `JOB_Job_ID` int NOT NULL ,
+    PRIMARY KEY (`APPOINTMENT_Appointment_ID`, `JOB_Job_ID`),
+    FOREIGN KEY (`APPOINTMENT_Appointment_ID`) REFERENCES `APPOINTMENT` (`Appointment_ID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-    FOREIGN KEY (`LAVORO_ID_Lavoro`) REFERENCES `LAVORO` (`ID_Lavoro`)
+    FOREIGN KEY (`JOB_Job_ID`) REFERENCES `JOB` (`Job_ID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION
 );
-CREATE TABLE `SVOLGE` (
-    `DIPENDENTE_N_Badge` int NOT NULL ,
-    `LAVORO_ID_Lavoro` int NOT NULL ,
-    PRIMARY KEY (`DIPENDENTE_N_Badge`, `LAVORO_ID_Lavoro`),
-    FOREIGN KEY (`DIPENDENTE_N_Badge`) REFERENCES `DIPENDENTE` (`N_Badge`)
+CREATE TABLE `PERFORMS` (
+    `EMPLOYEE_Badge_Number` int NOT NULL ,
+    `JOB_Job_ID` int NOT NULL ,
+    PRIMARY KEY (`EMPLOYEE_Badge_Number`, `JOB_Job_ID`),
+    FOREIGN KEY (`EMPLOYEE_Badge_Number`) REFERENCES `EMPLOYEE` (`Badge_Number`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-    FOREIGN KEY (`LAVORO_ID_Lavoro`) REFERENCES `LAVORO` (`ID_Lavoro`)
+    FOREIGN KEY (`JOB_Job_ID`) REFERENCES `JOB` (`Job_ID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION
 );
