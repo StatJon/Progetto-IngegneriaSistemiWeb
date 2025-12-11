a43f3ee7f99bc6433cd7b6fed65a61aa1f39eb5c
Jonathan Crescentini <jonathan.crescentini@studio.unibo.it>
2025-12-11 22:46:25 +0100

    feat: completed app.ts

--- EngineCheckProject/backend/src/app.ts
+++ EngineCheckProject/backend/src/app.ts
@@ -1,5 +1,8 @@
 import express, { Express } from "express"
-
+import customerRouter from "./routes/customer-router"
+import employeeRouter from "./routes/employee-router"
+import jobRouter from "./routes/job-router"
+import serviceRouter from "./routes/service-router"
 
 import historyApiFallback from "connect-history-api-fallback"
 
@@ -7,4 +10,21 @@ const app: Express = express()
 const port: number = 3000
 
 app.use(historyApiFallback())
+app.use(express.static("public")) //per risorse in public
 
+//routing api
+app.use(customerRouter)
+app.use(employeeRouter)
+app.use(jobRouter)
+app.use(serviceRouter)
+
+//fallback 404
+app.use(function(req, res, next) {
+  res.setHeader("Content-Type", "text/plain")
+  res.status(404).send("Ops... Pagina non trovata")
+})
+
+//start express
+app.listen(port, function() {
+  console.log(`Listening on http://localhost:${port}`)
+})
