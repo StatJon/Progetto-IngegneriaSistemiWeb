b8e0fec6908cbdb9dd98ab9cec5e611bc8a9a863
DibaFelix <hibahadine56@gmail.com>
2026-06-05 12:00:14 +0200

    added getServices() and deleteBooking()

--- EngineCheckProject/frontend/src/pages/EditBooking.vue
+++ EngineCheckProject/frontend/src/pages/EditBooking.vue
@@ -1,7 +1,52 @@
-<script setup lang="ts">
-import { ref, onMounted, computed } from 'vue';
-import { useRoute, useRouter } from 'vue-router';
+<script lang="ts">
+import { defineComponent } from 'vue';
+import type { Service, Booking } from '../types';
 import axios from 'axios';
+
+
+export default defineComponent({
+    data() {
+        return {
+          errorMessage: '',
+          services : [] as Service [],
+          selectedBooking : [] as Booking[],
+
+        }
+    },
+    async mounted() {
+      this.getServises();
+
+
+    },
+    methods: {
+      async getServises(){
+        try{
+          const response = await axios.get('/api/booking/');
+          
+
+        }catch(error: any){
+           this.errorMessage = error.response.data.message
+
+        }
+      },
+      async deleteBooking(){
+        try{
+
+        }catch(error: any )
+        {
+           this.errorMessage = error.response.data.message
+        }
+
+      },
+      async goBack() {
+      // tasto per tornare nella pagina precedente , senza salvare le cose
+      this.$router.back();
+    }
+
+
+    }
+}
+)
 
 const route = useRoute();
 const router = useRouter();
@@ -122,7 +167,7 @@ const handleAction = async () => {
     </div>
 
     <div class="action-container">
-      <button class="btn-cancel" @click="handleAction">
+      <button class="btn-cancel" @click="deleteBooking">
         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="margin-right:8px"><circle cx="12" cy="12" r="10"></circle><line x1="15" y1="9" x2="9" y2="15"></line><line x1="9" y1="9" x2="15" y2="15"></line></svg>
         {{ buttonText }}
       </button>
